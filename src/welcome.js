4cd77a6ea1adfd43f8b86687599b24e8eed150e9
Matt Duffield <matt.duffield@gmail.com>
2018-05-25 16:19:37 -0700

    Auto commit from FrontEnd Creator

--- src/welcome.js
+++ src/welcome.js
@@ -1,7 +1,7 @@
 //import {computedFrom} from 'aurelia-framework';
 
 export class Welcome {
-  heading = 'Welcome to the Aurelia Navigation App - 123';
+  heading = 'Welcome to the Aurelia Navigation App';
   firstName = 'John';
   lastName = 'Doe';
   previousValue = this.fullName;
